572f20cc23378e71cc3b6ddcf331951dbd2ade8d
pkshetty15 <pkshetty.15.11@gmail.com>
2024-02-04 20:17:00 +0530

    feat: predict crops

--- app/recommender.py
+++ app/recommender.py
@@ -9,3 +9,8 @@ with open('models/scaler.pkl', 'rb') as f:
 
 with open('models/cluster_data.pkl', 'rb') as f:
     cluster_data = pickle.load(f)
+
+def predict_crops(soil_data):
+    sample = model.predict(scaler.transform([soil_data]))
+    crops = cluster_data.loc[cluster_data['cluster'] == sample[0], 'label'].values
+    return set(crops)
